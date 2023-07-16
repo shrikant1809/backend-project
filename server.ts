a46e8a17bba6834ec505ec53424c3220f1666a95
shrikant patil <98108073+shrikant1809@users.noreply.github.com>
2023-07-16 09:42:22 +0530

    16july

--- server.ts
+++ server.ts
@@ -1,10 +1,8 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 
-const app = express();
-app.use(bodyParser.json());
-
 interface CourseOffering {
+  course_id: string;
   course_name: string;
   instructor_name: string;
   start_date: string;
@@ -12,7 +10,19 @@ interface CourseOffering {
   max_employees: number;
 }
 
+interface Registration {
+  registration_id: string;
+  email: string;
+  course_name: string;
+  course_id: string;
+  status: string;
+}
+
+const app = express();
+app.use(bodyParser.json());
+
 let courseOfferings: CourseOffering[] = [];
+let registrations: Registration[] = [];
 
 app.post('/add/courseOffering', (req: Request, res: Response) => {
   const {
@@ -45,6 +55,7 @@ app.post('/add/courseOffering', (req: Request, res: Response) => {
   const course_id = `OFFERING-${course_name}-${instructor_name}`;
 
   const newCourseOffering: CourseOffering = {
+    course_id,
     course_name,
     instructor_name,
     start_date,
@@ -82,7 +93,7 @@ app.post('/add/register/:course_id', (req: Request, res: Response) => {
   }
 
   const courseOffering = courseOfferings.find(
-    (offering) => offering.course_name === course_id
+    (offering) => offering.course_id === course_id
   );
 
   if (!courseOffering) {
@@ -109,6 +120,18 @@ app.post('/add/register/:course_id', (req: Request, res: Response) => {
     });
   }
 
+  const registration_id = `${employee_name}-${course_id}`;
+
+  const newRegistration: Registration = {
+    registration_id,
+    email,
+    course_name: courseOffering.course_name,
+    course_id,
+    status: 'ACCEPTED',
+  };
+
+  registrations.push(newRegistration);
+
   return res.status(200).json({
     status: 200,
     message: `successfully registered for ${course_id}`,
@@ -120,12 +143,24 @@ app.post('/add/register/:course_id', (req: Request, res: Response) => {
     },
   });
 });
-
-app.post('/allot/:course_id', (req: Request, res: Response) => {
+/*app.post('/add/register/:course_id', (req: Request, res: Response) => {
+  const { employee_name, email } = req.body;
   const { course_id } = req.params;
 
+  if (!employee_name || !email || !course_id) {
+    return res.status(400).json({
+      status: 400,
+      message: 'INPUT_DATA_ERROR',
+      data: {
+        failure: {
+          message: 'employee_name, email, course_id cannot be empty',
+        },
+      },
+    });
+  }
+
   const courseOffering = courseOfferings.find(
-    (offering) => offering.course_name === course_id
+    (offering) => offering.course_id === course_id
   );
 
   if (!courseOffering) {
@@ -140,11 +175,75 @@ app.post('/allot/:course_id', (req: Request, res: Response) => {
     });
   }
 
+  const existingRegistrations = registrations.filter(
+    (registration) => registration.course_id === course_id
+  );
+
+  if (courseOffering.max_employees <= courseOffering.min_employees) {
+    return res.status(400).json({
+      status: 400,
+      message: 'COURSE_FULL_ERROR',
+      data: {
+        failure: {
+          message: 'Cannot register for course, course is full',
+        },
+      },
+    });
+  }
+
+  const registration_id = `${employee_name}-${course_id}`;
+
+  const newRegistration: Registration = {
+    registration_id,
+    email,
+    course_name: courseOffering.course_name,
+    course_id,
+    status: 'ACCEPTED',
+  };
+
+  registrations.push(newRegistration);
+
+  return res.status(200).json({
+    status: 200,
+    message: `successfully registered for ${course_id}`,
+    data: {
+      success: {
+        registration_id,
+        status: 'ACCEPTED',
+      },
+    },
+  });
+});*/
+
+app.post('/allot/:course_id', (req: Request, res: Response) => {
+  const { course_id } = req.params;
+
+  const courseOffering = courseOfferings.find(
+    //(offering) => offering.course_name === course_id
+    (offering) => offering.course_id === course_id
+  );
+
+  if (!courseOffering) {
+    return res.status(400).json({
+      status: 400,
+      message: 'COURSE_NOT_FOUND',
+      data: {
+        failure: {
+          message: 'Course not found',
+        },
+      },
+    });
+  }
+
+  const allottedRegistrations = registrations.filter(
+    (registration) => registration.course_id === course_id
+  );
+
   return res.status(200).json({
     status: 200,
     message: 'successfully allotted course to registered employees',
     data: {
-      success: [],
+      success: allottedRegistrations,
     },
   });
 });
