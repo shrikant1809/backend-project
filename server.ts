2d68b505e506736ba382ff967ac869386c6a7f61
shrikant patil <98108073+shrikant1809@users.noreply.github.com>
2023-07-16 10:12:35 +0530

    test commit

--- server.ts
+++ server.ts
@@ -268,6 +268,9 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to the API ');
 });
 
+// app.listen(8000, () => {
+//   console.log('Server running on port 8000');
+// });
 app.listen(8000, () => {
   console.log('Server running on port 8000');
 });
