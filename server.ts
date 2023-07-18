82cff492c412121eb1559624b053859ded62cc48
shrikant patil <98108073+shrikant1809@users.noreply.github.com>
2023-07-18 11:57:00 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import mysql from 'mysql';
 
 interface CourseOffering {
   course_id: string;
@@ -20,6 +21,21 @@ interface Registration {
 
 const app = express();
 app.use(bodyParser.json());
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'qwerty1@',
+  database: 'taskdb1',
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+  } else {
+    console.log('Connected to MySQL database');
+  }
+});
 
 let courseOfferings: CourseOffering[] = [];
 let registrations: Registration[] = [];
@@ -63,16 +79,34 @@ app.post('/add/courseOffering', (req: Request, res: Response) => {
     max_employees,
   };
 
-  courseOfferings.push(newCourseOffering);
+  const query = 'INSERT INTO course_offerings SET ?';
+  connection.query(query, newCourseOffering, (err, result) => {
+    if (err) {
+      console.error('Error adding course offering:', err);
+      return res.status(500).json({
+        status: 500,
+        message: 'DATABASE_ERROR',
+        data: {
+          failure: {
+            message: 'Failed to add course offering',
+          },
+        },
+      });
+    }
 
-  return res.status(200).json({
-    status: 200,
-    message: 'course added successfully',
-    data: {
-      success: {
-        course_id,
+    console.log('Course offering added successfully');
+
+    courseOfferings.push(newCourseOffering);
+
+    return res.status(200).json({
+      status: 200,
+      message: 'course added successfully',
+      data: {
+        success: {
+          course_id,
+        },
       },
-    },
+    });
   });
 });
 
@@ -130,96 +164,42 @@ app.post('/add/register/:course_id', (req: Request, res: Response) => {
     status: 'ACCEPTED',
   };
 
-  registrations.push(newRegistration);
+  const query = 'SELECT * FROM taskdb1.registrations';
+  connection.query(query, newRegistration, (err, result) => {
+    if (err) {
+      console.error('Error adding registration:', err);
+      return res.status(500).json({
+        status: 500,
+        message: 'DATABASE_ERROR',
+        data: {
+          failure: {
+            message: 'Failed to add registration',
+          },
+        },
+      });
+    }
 
-  return res.status(200).json({
-    status: 200,
-    message: `successfully registered for ${course_id}`,
-    data: {
-      success: {
-        registration_id: `${employee_name}-${course_id}`,
-        status: 'ACCEPTED',
+    console.log('Registration added successfully');
+
+    registrations.push(newRegistration);
+
+    return res.status(200).json({
+      status: 200,
+      message: `successfully registered for ${course_id}`,
+      data: {
+        success: {
+          registration_id: `${employee_name}-${course_id}`,
+          status: 'ACCEPTED',
+        },
       },
-    },
+    });
   });
 });
-/*app.post('/add/register/:course_id', (req: Request, res: Response) => {
-  const { employee_name, email } = req.body;
-  const { course_id } = req.params;
-
-  if (!employee_name || !email || !course_id) {
-    return res.status(400).json({
-      status: 400,
-      message: 'INPUT_DATA_ERROR',
-      data: {
-        failure: {
-          message: 'employee_name, email, course_id cannot be empty',
-        },
-      },
-    });
-  }
-
-  const courseOffering = courseOfferings.find(
-    (offering) => offering.course_id === course_id
-  );
-
-  if (!courseOffering) {
-    return res.status(400).json({
-      status: 400,
-      message: 'COURSE_NOT_FOUND',
-      data: {
-        failure: {
-          message: 'Course not found',
-        },
-      },
-    });
-  }
-
-  const existingRegistrations = registrations.filter(
-    (registration) => registration.course_id === course_id
-  );
-
-  if (courseOffering.max_employees <= courseOffering.min_employees) {
-    return res.status(400).json({
-      status: 400,
-      message: 'COURSE_FULL_ERROR',
-      data: {
-        failure: {
-          message: 'Cannot register for course, course is full',
-        },
-      },
-    });
-  }
-
-  const registration_id = `${employee_name}-${course_id}`;
-
-  const newRegistration: Registration = {
-    registration_id,
-    email,
-    course_name: courseOffering.course_name,
-    course_id,
-    status: 'ACCEPTED',
-  };
-
-  registrations.push(newRegistration);
-
-  return res.status(200).json({
-    status: 200,
-    message: `successfully registered for ${course_id}`,
-    data: {
-      success: {
-        registration_id,
-        status: 'ACCEPTED',
-      },
-    },
-  });
-});*/
 
 app.post('/allot/:course_id', (req: Request, res: Response) => {
   const { course_id } = req.params;
 
   const courseOffering = courseOfferings.find(
-    //(offering) => offering.course_name === course_id
     (offering) => offering.course_id === course_id
   );
 
@@ -235,32 +215,77 @@ app.post('/allot/:course_id', (req: Request, res: Response) => {
     });
   }
 
-  const allottedRegistrations = registrations.filter(
-    (registration) => registration.course_id === course_id
-  );
+  const query = 'SELECT * FROM taskdb1.registrations';
+  connection.query(query, [course_id], (err, rows) => {
+    if (err) {
+      console.error('Error retrieving registrations:', err);
+      return res.status(500).json({
+        status: 500,
+        message: 'DATABASE_ERROR',
+        data: {
+          failure: {
+            message: 'Failed to retrieve registrations',
+          },
+        },
+      });
+    }
 
-  return res.status(200).json({
-    status: 200,
-    message: 'successfully allotted course to registered employees',
-    data: {
-      success: allottedRegistrations,
-    },
+    console.log('Registrations retrieved successfully');
+
+    const allottedRegistrations = rows;
+
+    return res.status(200).json({
+      status: 200,
+      message: 'successfully allotted course to registered employees',
+      data: {
+        success: allottedRegistrations,
+      },
+    });
   });
 });
 
 app.post('/cancel/:registration_id', (req: Request, res: Response) => {
   const { registration_id } = req.params;
 
-  return res.status(200).json({
-    status: 200,
-    message: `successfully cancelled registration for ${registration_id}`,
-    data: {
-      success: {
-        registration_id,
-        course_id: 'course id',
-        status: 'CANCEL_ACCEPTED',
+  const query = 'DELETE FROM registrations WHERE registration_id = ?';
+  connection.query(query, [registration_id], (err, result) => {
+    if (err) {
+      console.error('Error cancelling registration:', err);
+      return res.status(500).json({
+        status: 500,
+        message: 'DATABASE_ERROR',
+        data: {
+          failure: {
+            message: 'Failed to cancel registration',
+          },
+        },
+      });
+    }
+
+    console.log('Registration cancelled successfully');
+
+    return res.status(200).json({
+      status: 200,
+      message: `successfully cancelled registration for ${registration_id}`,
+      data: {
+        success: {
+          registration_id,
+          course_id: 'course id',
+          status: 'CANCEL_ACCEPTED',
+        },
       },
-    },
+    });
+  });
+});
+
+process.on('SIGINT', () => {
+  connection.end((err) => {
+    if (err) {
+      console.error('Error closing MySQL connection:', err);
+    } else {
+      console.log('MySQL connection closed');
+      process.exit();
+    }
   });
 });
 
@@ -268,9 +293,6 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to the API ');
 });
 
-// app.listen(8000, () => {
-//   console.log('Server running on port 8000');
-// });
 app.listen(8000, () => {
   console.log('Server running on port 8000');
 });
